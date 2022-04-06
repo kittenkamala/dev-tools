783c00d5608a1bf28748faf596d314eb8e514697
amy <amy.fiske@pantheon.io>
2022-04-06 09:14:52 -0700

    update db checks

--- wp-db-checks.sh
+++ wp-db-checks.sh
@@ -3,18 +3,18 @@
 source ./env-vars.sh
 
 ## MYSQL Health Checks ##
-mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -h $MYSQL_HOST -P 14622 $MYSQL_DATABASE --execute="
+mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -h $MYSQL_HOST -P 13727 $MYSQL_DATABASE --execute="
 show full processlist;
-show table status where engine = innodb;
-show table status where engine = myisam;"
+show table status where engine = 'innodb';
+show table status where engine = 'myisam';" > test.txt
 
 ##WP Specific db bloat checks##
-mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -h $MYSQL_HOST -P 14622 $MYSQL_DATABASE --execute="
+mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -h $MYSQL_HOST -P 13727 $MYSQL_DATABASE --execute="
 select * from $MYSQL_DB.$DB_PREFIX_options where option_name like '_wp_session_%';
 SELECT * FROM $DB_PREFIX_options WHERE autoload = 'yes';
 SELECT * FROM $DB_PREFIX_options WHERE autoload = 'yes' AND option_name like '%transient%';
-select * from $DB_PREFIX_posts where post_type = "revision";
-select * from $DB_PREFIX_posts where post_type = "trash";
+select * from $DB_PREFIX_posts where post_type = 'revision';
+select * from $DB_PREFIX_posts where post_type = 'trash';
 select * from from $DB_PREFIX_term_relationships where _term_taxonomy_id=1 and object_id not in (select id from $DB_PREFIX_posts);
 select * from $DB_PREFIX_postmeta as m left join prefix_posts as p on m.post_id = p.id where p.id is null;
 select * from $DB_PREFIX_comments  where comment_approved = 'spam';
