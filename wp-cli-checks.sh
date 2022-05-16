29610393c6376a654121075c509201d40ac4477a
amy <amy.fiske@pantheon.io>
2022-05-16 14:58:15 -0700

    update wp cli checks syntax + add verify checksums

--- wp-cli-checks.sh
+++ wp-cli-checks.sh
@@ -8,9 +8,11 @@ source ./env-vars.sh
 
 
 ## wp cli checks ##
-terminus wp $SITE.$ENV db size --size_format=mb #get db size
-terminus wp $SITE.$ENV plugin list #get plugin details
-terminus wp $SITE.$ENV theme list #get theme details
-terminus wp $SITE.$ENV core version
+terminus wp $SITE.$ENV -- db size --size_format=mb #get db size
+terminus wp $SITE.$ENV -- plugin list #get plugin details
+terminus wp $SITE.$ENV -- theme list #get theme details
+terminus wp $SITE.$ENV -- core version
+terminus wp $SITE.$ENV -- core verify-checksums
+terminus wp $SITE.$ENV -- plugin verify-checksums --all
 #check for transients
 #check cron health
