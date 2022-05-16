763d60e0f309814f63fcec6e2b5c43c4e222fc56
amy <amy.fiske@pantheon.io>
2022-05-16 12:39:10 -0700

    add WP user and pass to env-vars.sh

--- env-vars.sh
+++ env-vars.sh
@@ -2,14 +2,16 @@
 
 #set environment variables here
 
-export SQL_HOST="dbserver.dev.43942568-b4db-498b-9fc9-511b84eae547.drush.in"
+export SQL_HOST=""
 export SQL_USER="pantheon"
-export SQL_PASS="9bfae9b87d8a4bd9a6430cada5037f49"
+export SQL_PASS=""
 export SQL_DB="pantheon"
-export DB_PREFIX="wp"
+export DB_PREFIX="wp_"
 export TOKEN="terminus machine token"
 export EMAIL="email for terminus auth"
 export ENV="environment name here"
 export SITE=""
 export DOMAIN=""
 export WPS_TOKEN="WPScan access token here"
+export WP_USER="pantheon"
+export WP_PASS=""
